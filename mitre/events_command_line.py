ae2663f0d904036d8dbad7274fb7a44071ec4c66
Eli Bogart <ebogart@partners.org>
2017-06-15 12:44:06 -0400

    First draft of code and data for the time-dependent-outcome problem

---
+++ mitre/events_command_line.py
@@ -0,0 +1,20 @@
+""" 
+Entry point for running MITRE_EVENTS from the command line.
+
+Calls mitre.events.run_from_config_file on its first argument; we set this
+up using argparse so that a sensible usage message can be displayed.
+
+"""
+
+import os
+import argparse
+import mitre.main
+
+def run():
+    parser = argparse.ArgumentParser(
+        description='Launch MITRE_EVENTS calculations as specified in configuration file.',
+        epilog='See MITRE documentation and the draft MITRE_EVENTS writeup for details on available operations and '
+               'how to specify them in the configuration file format.')
+    parser.add_argument('config_file',type=str,help='configuration file to read')
+    args = parser.parse_args()
+    mitre.events.run_from_config_file(args.config_file)
